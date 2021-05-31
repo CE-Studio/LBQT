116a7e8afecde4a846676e947b3b8e921f365229
clarence112 <clarencepacelli@gmail.com>
2021-05-30 20:32:59 -0400

    no message

--- LowBudgetQT/Assets/Scripts/doorController.cs
+++ LowBudgetQT/Assets/Scripts/doorController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class doorController : MonoBehaviour, receiver {
     private Animator anim;
+    private AudioSource sound;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        sound = GetComponent<AudioSource>();
     }
 
     public void setmode(bool inp) {
         anim.SetBool("open", inp);
+        if (inp) {
+            sound.Play();
+        }
     }
 }
