3c59bc8ced26058f750371b1f886283c70f756ad
clarence112 <clarencepacelli@gmail.com>
2021-05-31 23:37:49 -0400

    no message

---
+++ LowBudgetQT/Assets/Scripts/antiSleep.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class antiSleep : MonoBehaviour
+{
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       rb.WakeUp();
+    }
+}
