300115471c885bb955a1984050a81861386a6124
clarence112 <clarencepacelli@gmail.com>
2021-06-04 01:35:09 -0400

    no message

--- LowBudgetQT/Assets/Scripts/elevatorController.cs
+++ LowBudgetQT/Assets/Scripts/elevatorController.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class elevatorController:MonoBehaviour {
     public bool canRun = true;
     public Animator anim;
+    private bool running = false;
 
     // Start is called before the first frame update
     void Start() {
-
+        anim.SetBool("open", true);
     }
 
     // Update is called once per frame
     void Update() {
-
+        if (running) {
+            anim.SetBool("open", false);
+            transform.position = new Vector3(transform.position.x, transform.position.y + (Time.deltaTime / 2.0f), transform.position.z);
+        }
     }
 
     void OnTriggerEnter(Collider other) {
-        if (other.tag == "Player" && (!oneshot || !ran)) {
+        if (canRun && (other.tag == "Player")) {
+            running = true;
         }
     }
 }
