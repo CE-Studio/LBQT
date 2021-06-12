d183b28a2f33ad3daa67edcb62afd5aa22ebce16
clarence112 <clarencepacelli@gmail.com>
2021-06-11 22:20:22 -0400

    no message

---
+++ LowBudgetQT/Assets/Scripts/antlineController1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class antlineController1:MonoBehaviour {
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
