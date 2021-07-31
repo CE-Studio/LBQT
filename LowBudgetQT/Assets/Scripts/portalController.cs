8c418501d016bdc6c8ddcd6fa352fae92d6b19ce
clarence112 <clarencepacelli@gmail.com>
2021-07-30 22:00:09 -0400

    no message

--- LowBudgetQT/Assets/Scripts/portalController.cs
+++ LowBudgetQT/Assets/Scripts/portalController.cs
@@ -2,23 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class portalController : MonoBehaviour
-{
+public class portalController:MonoBehaviour {
 
     public Transform player;
     public Transform tracker;
+    public Transform point;
     public Transform camera;
+    public Camera portalCam;
+    private Camera playerCam;
+    private float nearClipOffset = 0.0f;
+    private float nearClipLimit = 0.0f;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        playerCam = player.GetComponent<Camera>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         (tracker.position, tracker.rotation) = (player.position, player.rotation);
         (camera.localPosition, camera.localRotation) = (tracker.localPosition, tracker.localRotation);
+        //SetNearClipPlane();
+    }
+
+
+    //https://github.com/SebLague/Portals/blob/master/Assets/Scripts/Core/Portal.cs
+    void SetNearClipPlane() {
+        // Learning resource:
+        // http://www.terathon.com/lengyel/Lengyel-Oblique.pdf
+        Transform clipPlane = point;
+        int dot = System.Math.Sign(Vector3.Dot(clipPlane.forward, transform.position - portalCam.transform.position));
+
+        Vector3 camSpacePos = portalCam.worldToCameraMatrix.MultiplyPoint(clipPlane.position);
+        Vector3 camSpaceNormal = portalCam.worldToCameraMatrix.MultiplyVector(clipPlane.forward) * dot;
+        float camSpaceDst = -Vector3.Dot(camSpacePos, camSpaceNormal) + nearClipOffset;
+
+        // Don't use oblique clip plane if very close to portal as it seems this can cause some visual artifacts
+        if (Mathf.Abs(camSpaceDst) > nearClipLimit) {
+            Vector4 clipPlaneCameraSpace = new Vector4(camSpaceNormal.x, camSpaceNormal.y, camSpaceNormal.z, camSpaceDst);
+
+            // Update projection based on new clip plane
+            // Calculate matrix with player cam so that player camera settings (fov, etc) are used
+            portalCam.projectionMatrix = playerCam.CalculateObliqueMatrix(clipPlaneCameraSpace);
+        } else {
+            portalCam.projectionMatrix = playerCam.projectionMatrix;
+        }
     }
 }
